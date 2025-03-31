e198ca79c042bffc484662c884713e21f44d5884
caioxdalaud <lindo.gostoso@gamil.com>
2025-03-31 14:33:38 -0300

    yeah man continue

---
+++ while/while atv 2.py
@@ -0,0 +1,9 @@
+numsitivo = float(input("digite um numero positivo: "))
+
+while numsitivo >= 0:
+    print("seu numero é positivo")
+    break
+while numsitivo < 0:
+    numsitivo = float(input("digite novamente: "))
+if numsitivo >= 0:
+    print("seu numero é postivo")
---
+++ while/while atv.py
@@ -0,0 +1,16 @@
+numero = int(input("digite um numero misterioso:  "))
+
+while numero == 0:
+    print("correto")
+    break
+while numero > 0:
+    numero -= 1
+    print(numero)
+while numero < 0:
+    numero += 1
+    print(numero)
+
+
+
+
+
