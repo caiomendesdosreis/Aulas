2d69d4e0cbbc1b6d0c69ef9bb9fcd837ce696e35
caioxdalaud <lindo.gostoso@gamil.com>
2025-04-07 12:50:14 -0300

    mais vetores

---
+++ vetores/numeros.py
@@ -0,0 +1,7 @@
+numeros = [3, 8, 5 ,9]
+numeros.sort()
+print(numeros)
+
+nomes = ["carlos", "ana", "bruno"]
+nomes.sort()
+print(nomes)
---
+++ vetores/numerosReverso.py
@@ -0,0 +1,7 @@
+numeros = [3, 1, 4, 2]
+numeros.sort(reverse=True)
+print(numeros)
+
+nomes = ["carlos", "ana", "berinbal"]
+nomes.sort(reverse=True)
+print(nomes)
